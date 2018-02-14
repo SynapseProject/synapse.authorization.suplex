b94075a4b0ba54171ea3987839421c68ab8e5c1f
Steve <steve.shortt@gmail.com>
2018-02-14 11:12:13 -0600

    setup versioning script and revved to 1.0

--- Synapse.Authorization.Suplex/Properties/AssemblyInfo.cs
+++ Synapse.Authorization.Suplex/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Synapse.Authorization.Suplex")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Synapse.Authorization.Suplex")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "Synapse.Authorization.Suplex" )]
+[assembly: AssemblyDescription( "Suplex Authorization provider for Synapse.Server" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Synapse.Authorization.Suplex" )]
+[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016 - 2018" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fb5bcb39-ebe1-4eed-96e2-771fcaae9093")]
+[assembly: Guid( "fb5bcb39-ebe1-4eed-96e2-771fcaae9093" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.18045.0" )]
